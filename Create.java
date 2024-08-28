70f5d13e7f13c57fb063307c42a664a66144ade1
saiachyuth421 <saiachyuth2222@gmail.com>
2024-08-28 11:32:28 +0530

    Sai

--- Create.java
+++ Create.java
@@ -1,7 +1,19 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class Create{
 public static void main(String[]args){
 // Create an Object for ArrayList
   // Load Values in ArrayList
   // Iterate it by usig Iterator and While Loops
+  List<Integer> l=new ArrayList<>();
+l.add(1);
+l.add(2);
+l.add(3);
+Iterator i=l.iterator();
+while (i.hasNext()) {
+  System.out.println(i.next());
+}
 }
 }
